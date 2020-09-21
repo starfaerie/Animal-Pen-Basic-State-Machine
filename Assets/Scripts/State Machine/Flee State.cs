b49868261501fc88e88aaa15c49dd95349b3ec11
starfaerie <54830622+starfaerie@users.noreply.github.com>
2020-09-21 03:08:00 -0600

    Fixed the States (second time)

--- Assets/Scripts/State Machine/Flee State.cs
+++ Assets/Scripts/State Machine/Flee State.cs
@@ -15,25 +15,37 @@ namespace State_Machine
         {
             if (!StateController.CheckIfInRange())
             {
+                StateController.ResetPatrolPoints();
                 StateController.SetState(new PatrolState(StateController));
+            }
+
+            if (!(Vector3.Distance(StateController.transform.position, StateController.enemyToChase.transform.position) > StateController.tagRadius))
+            {
+                StateController.SetState(new ChaseState(StateController));
             }
         }
         public override void Act()
         {
-            _destination = StateController.getChased();
+            _destination = StateController.getFlee();
             StateController.ai.SetTarget(_destination);
         }
 
         public override void OnStateEnter()
         {
+            StateController.flee = true;
+            
             _destination = StateController.getChased();
             if(StateController.ai.agent != null)
             {
-                StateController.ai.agent.speed = 1f;
+                StateController.ai.agent.speed = 1.5f;
             }
             StateController.ai.SetTarget(_destination);
             StateController.ChangeColor(Color.red);
+        }
 
+        public override void OnStateExit()
+        {
+            StateController.flee = false;
         }
     }
 }
--- Assets/Scripts/State Machine/StateController.cs
+++ Assets/Scripts/State Machine/StateController.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace State_Machine
 {
@@ -6,10 +10,11 @@ namespace State_Machine
     [RequireComponent(typeof(UnityStandardAssets.Characters.ThirdPerson.AICharacterControl))]
     public class StateController : MonoBehaviour
     {
-        //public GameObject[] randPoints; //random points the AI travels to when fleeing
-        public GameObject[] patrolPoints; //patrol points of this AI
+        [HideInInspector]
+        public Vector3[] patrolPoints; //patrol points of this AI
         public GameObject[] enemies; //enemies of this AI
-        public float detectionRadius = 5f; //detection radius of this AI
+        public float detectionRadius = 8f; //detection radius of this AI
+        public float tagRadius = 2f;
 
         [HideInInspector]
         public UnityStandardAssets.Characters.ThirdPerson.AICharacterControl ai;
@@ -20,24 +25,26 @@ namespace State_Machine
         [HideInInspector]
         public State currentState;
         private int _patrolPointNum;
-        //private int _randPoint;
+        [HideInInspector]
+        public bool flee;
 
         public Vector3 GetNextNavPoint()
         {
-            _patrolPointNum = (_patrolPointNum + 1) % patrolPoints.Length; //iterates through all patrol points, moving to the next point each time this method is called, automagically loops back to the first point when it runs out of points
-            return patrolPoints[_patrolPointNum].transform.position; //returns the position of the next patrol point
+            _patrolPointNum = _patrolPointNum = (_patrolPointNum + 1) % patrolPoints.Length; //iterates through all patrol points, moving to the next point each time this method is called, automagically loops back to the first point when it runs out of points
+            return patrolPoints[_patrolPointNum]; //returns the position of the next patrol point
         }
         public Vector3 getChased()
         {
             _patrolPointNum = (_patrolPointNum + 1) % patrolPoints.Length; //iterates through all patrol points, moving to the next point each time this method is called, automagically loops back to the first point when it runs out of points
-            return patrolPoints[_patrolPointNum].transform.position; //returns the position of the next patrol point
+            return patrolPoints[_patrolPointNum]; //returns the position of the next patrol point
         }
 
-        /*public Transform GetNextRandPoint()
+        public Vector3 getFlee()
         {
-            _randPoint = transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
-            return randPoints[_randPoint].transform;
-        */
+            Vector3 currentPos = gameObject.transform.position;
+            Vector3 fleeDirection = (currentPos - enemyToChase.transform.position).normalized;
+            return currentPos + fleeDirection * 3f;
+        }
 
         public void ChangeColor(Color color)
         {
@@ -53,11 +60,23 @@ namespace State_Machine
         public bool CheckIfInRange()
         {
             if (enemies == null) return false;
-        
+
+            if (flee == false)
+            {
+                foreach (GameObject go in enemies)
+                {
+                    if (!(Vector3.Distance(go.transform.position, transform.position) < detectionRadius)) continue;
+
+                    enemyToChase = go;
+                    return true;
+                }
+                return false;
+            }
+            
             foreach (GameObject go in enemies)
             {
-                if (!(Vector3.Distance(go.transform.position, transform.position) < detectionRadius)) continue;
-            
+                if (!(Vector3.Distance(go.transform.position, transform.position) < detectionRadius * 1.5f)) continue;
+
                 enemyToChase = go;
                 return true;
             }
@@ -68,8 +87,39 @@ namespace State_Machine
         {
             ai = GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControl>();
             childrenRend = GetComponentsInChildren<Renderer>();
+            enemyToChase = enemies[0].gameObject;
+
+            ResetPatrolPoints();
 
             SetState(new PatrolState(this)); //sets the first state to the PatrolState
+        }
+
+        public void ResetPatrolPoints()
+        {
+            //get 4 random numbers in a given range
+            float rand0 = Random.Range(-10f, 10f);
+            float rand1 = Random.Range(-10f, 0f) + Mathf.Abs(rand0);
+            float rand2 = Random.Range(0f, 10f) + Mathf.Abs(rand1);
+            float rand3 = Random.Range(1f, 4f);
+            
+            //get the AI and enemy's position
+            Vector3 currentPos = transform.position;
+            Vector3 enemyPos = enemyToChase.transform.position;
+            Vector3 enemyDir = (enemyPos - currentPos).normalized * 3f;
+
+            //assign 9 new patrol points
+            patrolPoints = new[]
+            {
+                new Vector3(rand0, 0, rand0) - enemyDir * rand3,
+                new Vector3(rand0, 0, rand1) - enemyDir * rand3,
+                new Vector3(rand0, 0, rand2) - enemyDir * rand3,
+                new Vector3(rand1, 0, rand0) - enemyDir * rand3,
+                new Vector3(rand1, 0, rand1) - enemyDir * rand3,
+                new Vector3(rand1, 0, rand2) - enemyDir * rand3,
+                new Vector3(rand2, 0, rand0) - enemyDir * rand3,
+                new Vector3(rand2, 0, rand1) - enemyDir * rand3,
+                new Vector3(rand2, 0, rand2) - enemyDir * rand3
+            };
         }
 
         private void Start()
